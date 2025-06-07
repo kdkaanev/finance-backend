698a8f7c53638322d6181fd9a5a9ec2b750a95c8
kdkaanev <kdkaane@github.io>
2025-06-07 12:56:56 +0300

    sumary transactions finall

--- backend/core_app/admin.py
+++ backend/core_app/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
+from backend.core_app.models import Transactions, Budget, Pots
+
+@admin.register(Transactions)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'amount', 'is_recurring', 'user')
+    search_fields = ('name', 'type')
+    list_filter = ('type', 'is_recurring', 'user')
+    ordering = ('-id',)
+
+
+@admin.register(Budget)
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('category', 'maximum', 'theme')
+    search_fields = ('category',)
+    list_filter = ('category', 'theme')
+    ordering = ('-id',)
+
+
+@admin.register(Pots)
+class PotAdmin(admin.ModelAdmin):
+    list_display = ('name', 'target', 'total', 'theme', 'user')
+    search_fields = ('name',)
+    list_filter = ('theme', 'user')
+    ordering = ('-id',)
